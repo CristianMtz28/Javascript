1ba67af3b1f9876b2baa550497002ea7908831ed
Cristian <cristian.martinez.kodistos@gmail.com>
2019-02-14 21:50:03 -0600

    Nuestro primer commit de Javascript

---
+++ Javascript/01-HolaMundo.js
@@ -0,0 +1,19 @@
+alert("Hola Mundo con Javascript");
+document.write("Usando el document.write"); 
+
+var number = 2;
+var number2 = 2;
+
+if(number >= 2){
+    alert("Soy un número mayor a 2");
+    return;
+}
+
+if(number2 >= 2){
+    alert("Soy el número 2");
+    return;
+}
+
+if(number2 && number = 2){
+    alert(number + number2);
+}
