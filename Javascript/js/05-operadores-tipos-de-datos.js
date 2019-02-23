12ddfc2471360b11983f548826217bcddb4aca02
Cristian <cristian.martinez.kodistos@gmail.com>
2019-02-22 21:02:47 -0600

    Siguiendo el curso

--- Javascript/js/05-operadores-tipos-de-datos.js
+++ Javascript/js/05-operadores-tipos-de-datos.js
@@ -1,5 +1,20 @@
 'use strict'
 
 /* OPERADORES & TIPOS DE DATOS
-
+OPERADORES
+Los operadores dentro de un lenguaje de programación es lo mismo
+que un operador matemático, por ejemplo:
 */
+
+var number1 = 7;
+var number2 = 10;
+
+var addition = number1 + number2;
+var subtraction =  number1 - number2;
+var multiplication = number1 * number2;
+var division = number1 / number2;
+
+alert('El resultado de la suma es: ' + addition);
+alert('El resultado de la resta es: ' + subtraction);
+alert('El resultado de la multiplicación es: ' + multiplication);
+alert('El resultado de la division es: ' + division);
