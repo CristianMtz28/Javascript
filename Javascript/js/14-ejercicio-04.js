f0d728ad42c5588173a53af93da85aa3c574c6f5
Cristian <cristianmtz28@hotmail.com>
2019-05-09 17:13:47 -0500

    Ejercicio 4

---
+++ Javascript/js/14-ejercicio-04.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/*EJERCICIO 04*/
+
+/*
+Mostrar los números impares que estén entre dos números introducios por el usuario
+*/
+
+var number1 = parseInt(prompt('Introduce el primer número', 0));
+var number2 = parseInt(prompt('Introduce el segundo número', 0));
+
+/*Código encontrado en la clase del curso , pasó un rato cómo 2 meses que programé así que veré el vídeo
+y replicaré el código para que pueda tener un "refresh" en mi memoria de cómo programar*/
+
+document.write("Los números impares entre " + number1 + " y " + number2 + " son:");
+
+for(var i=number1; i <=number2; i++){
+    if (i%2!=0) {
+        document.write(i+"</br>");
+    }
+}
+
+
+/*Este wey se rifó bien cabron y tiene una lógica bien simple:*/
+
+/*Con el for hace un ciclo para pedi  */
